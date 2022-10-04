3f0d780d9119af43a503dba7525fc6e44656f04b
diegogglez <diegomiller7@gmail.com>
2022-10-04 10:10:30 +0200

    Corrección Iteración #3: Operadores

--- upgrade-basic-algorithms/operadores.js
+++ upgrade-basic-algorithms/operadores.js
@@ -1,14 +1,14 @@
 // 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
 
-alert(10 * 5);
+console.log(10 * 5);
 
 // 1.2 Divide 10 por 2 y muestra el resultado en un alert.
 
-alert(10 / 2);
+console.log(10 / 2);
 
 // 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
 
-alert(15 / 9);
+console.log(15 % 9);
 
 // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 //teniendo dos variables y = 10 y z = 5.
