1c7274887861f6022600e5fc599bede8a8aedff1
diegogglez <diegomiller7@gmail.com>
2022-10-04 11:27:38 +0200

    Iteración #1: Buscar el máximo

---
+++ upgrade-basic-functions/iteration1.js
@@ -0,0 +1,12 @@
+// **Iteración #1: Buscar el máximo**
+
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+  if(numberOne > numberTwo){
+    console.log(numberOne);
+  } else console.log(numberTwo);
+}
+
+sum(100,29);
+
