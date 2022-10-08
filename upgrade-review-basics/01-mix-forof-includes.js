64989a98bbbb284b47bbd866dc59b6b33099efd3
diegogglez <diegomiller7@gmail.com>
2022-10-08 13:00:01 +0200

    Iteración #1: Mix forof and includes

---
+++ upgrade-review-basics/01-mix-forof-includes.js
@@ -0,0 +1,23 @@
+// Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las 
+// películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. 
+// Para filtrar las categorías puedes ayudarte de la función .includes()
+
+const movies = [
+  {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+  {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+  {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+  {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+
+const movieGenders = [];
+
+for (let item of movies) {
+  for (let categorie of item.categories) {
+    if (!movieGenders.includes(categorie)) {
+      movieGenders.push(categorie);
+    }
+  } 
+}
+
+console.log(movieGenders);
