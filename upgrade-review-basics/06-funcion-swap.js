9bd0c53edc7000a4f4f7f6df25d52d1a17115248
diegogglez <diegomiller7@gmail.com>
2022-10-09 19:52:55 +0200

    Iteración #6: Función swap

---
+++ upgrade-review-basics/06-funcion-swap.js
@@ -0,0 +1,18 @@
+// Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. 
+// La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. 
+// Retorna el array resultante.
+
+// Sugerencia de array:
+
+const players = ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño'];
+
+function swapItems(arr, indexA, indexB) {
+  let itemB = arr[indexA]
+  arr[indexA] = arr[indexB];
+  arr[indexB] = itemB;
+  return arr;
+}
+console.log(players);
+console.log(swapItems(players, 2, 3));
+
+
