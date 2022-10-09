08373fcc8ab0b196afca6bb454ab2e5702851676
diegogglez <diegomiller7@gmail.com>
2022-10-09 19:05:12 +0200

    Iteración #4: Métodos findArrayIndex

---
+++ upgrade-review-basics/04-metodos-findArrayIndex.js
@@ -0,0 +1,22 @@
+// Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y un texto 
+// y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste 
+// como parametro. Haz varios ejemplos y compruebalos.
+
+// Sugerencia de función:
+
+const animals = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+
+function findArrayIndex(array, text) {
+  array.forEach((item, i) => {
+    if (item === text) {
+      console.log(i + ': ' + item);
+      //*return i; por qué me da undefined???
+    }
+  })
+}
+
+/* findArrayIndex(animals, 'Salamandra') */
+findArrayIndex(animals, 'Caracol');
+findArrayIndex(animals, 'Mosquito');
+findArrayIndex(animals, 'Salamandra');
+findArrayIndex(animals, 'Ajolote');
